898444fd342474e55ba61aad266578473a5ed4b1
MashrafiGit <mdmasrafi902@gmail.com>
2025-08-30 17:39:50 +0600

    findOut middle of the linked list

---
+++ middle_of_the_linked_list.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    int size(ListNode *head)
+    {
+        ListNode *tmp = head;
+        int sz = 0;
+        while (tmp != NULL)
+        {
+            sz++;
+            tmp = tmp->next;
+        }
+        return sz;
+    }
+    ListNode *middleNode(ListNode *head)
+    {
+        int sz = size(head);
+
+        ListNode *tmp = head;
+        for (int i = 1; i <= sz / 2; i++)
+        {
+            tmp = tmp->next;
+        }
+
+        return tmp;
+    }
+};
